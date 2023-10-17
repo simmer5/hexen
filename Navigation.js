ec52d58398732479534a70e4d86a8ea8a17538e2
Mindaugas <strumke@yahoo.com>
2023-10-17 23:38:37 +0200

    pridejau analitics

---
+++ Navigation.js
@@ -0,0 +1,11 @@
+import Link from 'next/link'
+
+const Navigation = () => {
+	return (
+		<nav>
+			<Link href='/apie'>Apie</Link>
+		</nav>
+	)
+}
+
+export default Navigation
--- app/page.js
+++ app/page.js
@@ -3,7 +3,14 @@ import Image from 'next/image'
 export default function Home() {
 	return (
 		<main className='flex min-h-screen flex-col items-center justify-evenly p-8'>
-			{/* <div className='z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex'></div> */}
+			<div className='flex z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex'>
+				<a href='/apie'>
+					<p>Apie</p>
+				</a>
+				<a href='/kontaktai'>
+					<p>Kontaktai</p>
+				</a>
+			</div>
 
 			<div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
 				<div>
@@ -14,7 +21,7 @@ export default function Home() {
 				</div>
 			</div>
 
-			<div className='mb-22 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left'>
+			<div className='mb-22 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-3 lg:text-left justify-items-center'>
 				<a
 					href='#'
 					className='group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30'
@@ -62,7 +69,8 @@ export default function Home() {
 						Korpusinių baldų montavimas
 					</p>
 				</a>
-
+			</div>
+			<div className='mb-22 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-2 lg:text-left justify-items-center'>
 				<a
 					href='#'
 					className='group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30'
@@ -76,6 +84,21 @@ export default function Home() {
 					</h2>
 					<p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
 						Pakabinamų lubų montavimas
+					</p>
+				</a>
+				<a
+					href='#'
+					className='group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30'
+					rel='noopener noreferrer'
+				>
+					<h2 className={`mb-3 text-1xl font-semibold`}>
+						Vidaus durys{' '}
+						<span className='inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none'>
+							-&gt;
+						</span>
+					</h2>
+					<p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
+						Vidaus durų montavimas
 					</p>
 				</a>
 			</div>
